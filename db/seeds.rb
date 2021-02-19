4c768ca69fdb72e9edb821af130d5dd6a1b3b63f
thomas messett <thomas.messett@stampapp.me>
2021-02-19 12:02:22 +0000

    seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,18 @@ require 'json'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-# ingredients_serialized = open(url).read
-# ingredients = JSON.parse(ingredients_serialized)
-# drinks = ingredients["drinks"]
-# drinks.each do |drink|
-#   i = drink['strIngredient1']
-#   Ingredient.create(name: i)
-# end
+ url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ ingredients_serialized = open(url).read
+ ingredients = JSON.parse(ingredients_serialized)
+ drinks = ingredients["drinks"]
+ drinks.each do |drink|
+   i = drink['strIngredient1']
+   Ingredient.create(name: i)
+end
 
-# Cocktail.create(name: "Manhattan")
-# Cocktail.create(name: "Martini")
-# Cocktail.create(name: "Espresso Martini")
+Cocktail.create(name: "Manhattan")
+Cocktail.create(name: "Martini")
+Cocktail.create(name: "Espresso Martini")
 
 Dose.create(description: "2 shots", ingredient_id: 1, cocktail_id: 1)
 Dose.create(description: "1 shot", ingredient_id: 3, cocktail_id: 1)
